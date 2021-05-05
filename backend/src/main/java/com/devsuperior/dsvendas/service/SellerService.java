c1414ae45e12af53bd36d0d6f98b81b94a50398f
Danilo Castro <danilotrindade87@gmail.com>
2021-05-05 20:19:25 -0300

    Pagination

--- backend/src/main/java/com/devsuperior/dsvendas/service/SellerService.java
+++ backend/src/main/java/com/devsuperior/dsvendas/service/SellerService.java
@@ -1,7 +1,6 @@
 package com.devsuperior.dsvendas.service;
 
 import java.util.List;
-import java.util.stream.Collector;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -24,5 +23,4 @@ public class SellerService {
 		List<Seller> result = repository.findAll();
 		return result.stream().map(x -> new SellerDTO(x)).collect(Collectors.toList());
 	}
-	
 }
