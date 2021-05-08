d86cc816db9067a21742021f7c4323c143231a13
Danilo Castro <danilotrindade87@gmail.com>
2021-05-08 13:51:19 -0300

    Env var

--- frontend/src/utils/requests.ts
+++ frontend/src/utils/requests.ts
@@ -1,1 +1,3 @@
-export const BASE_URL = 'http://localhost:8080';
+// a base_url está definida para usar a variavel de ambiente definida online (netlify)
+// e a também usará a variável local, caso não esteja definida online.
+export const BASE_URL = process.env.REACT_APP_BACKEND_URL ?? 'http://localhost:8080';
